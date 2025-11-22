ce49cbffba214436633884135783b3b6440b4b4f
Ding <charlie.ding@ontario.ca>
2025-11-22 16:46:21 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import ChineseFlashcardApp from './components/ChineseFlashcards'
+// import ChineseFlashcardApp from './components/ChineseFlashcard'
+
+import ChineseFlashcardApp from './components/update/version1/ChineseFlashcard'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChineseFlashcardApp/>
+  </StrictMode>,
+)
